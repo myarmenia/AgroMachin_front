e364561abffcd4fd4fcc7a8b2445a27834bf90ea
Edgar <edgaravetisyan932@gmail.com>
2023-02-24 17:27:23 +0400

    created main modal

--- js/index.js
+++ js/index.js
@@ -20,3 +20,56 @@ sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
   header.classList.toggle("header-active");
 });
+
+// ------------------------------------------------------------------
+// Modal toggling
+// ------------------------------------------------------------------
+const modal_container = document.querySelector(".modal-container");
+function showModal(name) {
+  console.log("show");
+  const modal = document.querySelector(name);
+
+  document.body.classList.add("scroll-disabled");
+  modal_container.style.display = "block";
+  modal.style.display = "block";
+
+  setTimeout(() => {
+    modal.classList.remove("modal-close");
+    modal_container.classList.remove("modal-close");
+    modal_container.classList.add("modal-open");
+    modal.classList.add("modal-open");
+  }, 1);
+
+  // if (modal.offsetTop + 1000 < 20) {
+  //   modal.classList.add("modal-open-sm");
+  // } else {
+  // }
+}
+function hideModal() {
+  console.log("hide");
+  const modals = document.getElementsByClassName("modal");
+  for (let i = 0; i < modals.length; i++) {
+    const modal = modals[i];
+
+    modal.classList.remove("modal-open", "modal-open-sm");
+    modal_container.classList.remove("modal-open");
+
+    modal_container.classList.add("modal-close");
+    modal.classList.add("modal-close");
+    setTimeout(() => {
+      modal_container.style.display = "none";
+      modal.style.display = "none";
+      document.body.classList.remove("scroll-disabled");
+    }, 300);
+  }
+}
+document.querySelectorAll(".modal .close").forEach((el) => {
+  el.addEventListener("click", hideModal);
+});
+window.addEventListener("click", (e) => {
+  if (e.target == modal_container) {
+    hideModal();
+  }
+});
+
+// ==================================================================
