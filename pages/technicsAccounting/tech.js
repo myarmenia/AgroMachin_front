098c924774524e3005299c5a125b6a0565c16019
Rafo Aydinyan <raf.aydinyan98@gmail.com>
2023-02-27 16:37:29 +0400

    fonts

--- pages/technicsAccounting/tech.js
+++ pages/technicsAccounting/tech.js
@@ -3,9 +3,10 @@ const inp2 = document.querySelector('.documents');
 const file = document.querySelector('#file');
 const subscript = document.getElementById('subscript');
 const subscriptImg = document.querySelector('.subscript_img');
-const documentsImg = document.getElementsByClassName('documents_img')[0];
+const documentsImg = document.getElementsByClassName('documents_img')[0]
 const deleteImg = document.getElementsByClassName('delete')[0];
 const deleteImg2 = document.getElementsByClassName('delete2')[0];
+const form =document.getElementById("form")
 inp1.onclick = function() {
     subscript.click()
 }
@@ -33,3 +34,15 @@ deleteImg2.addEventListener('click', function() {
     documentsImg.style.display = 'none';
     inp2.style.display = 'flex';
 })
+
+
+function getData(form) {
+    console.log("test");
+    const formData = new FormData(form);
+    console.log(Object.fromEntries(formData));
+  }
+  
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    getData(e.target);
+  });
