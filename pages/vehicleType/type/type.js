971fd5bc9cb7a9cd343aee285aa82a54063039a6
rubsargsyann <ruben.sargsyan20012018@gmail.com>
2023-03-11 12:46:28 +0400

    changesEdgarRubo__

--- pages/vehicleType/type/type.js
+++ pages/vehicleType/type/type.js
@@ -4,11 +4,11 @@ const new_vehicle = document.getElementsByClassName("new_vehicle")[0],
   const change_record_btn = document.querySelector("#change_record");
 function articleToggle() {
   new_vehicle2.classList.remove("new_vehicle_show2");
-  new_vehicle.classList.toggle("new_vehicle_show");
+  new_vehicle.classList.add("new_vehicle_show");
 }
 function articleChangeToggle() {
   new_vehicle.classList.remove("new_vehicle_show");
-  new_vehicle2.classList.toggle("new_vehicle_show2");
+  new_vehicle2.classList.add("new_vehicle_show2");
 }
 new_record_btn.addEventListener("click", articleToggle);
 change_record_btn.addEventListener("click", articleChangeToggle);
