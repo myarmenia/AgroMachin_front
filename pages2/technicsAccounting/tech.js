28cddf05e06abe28dabd8ea843415a3ac247f7a4
Edgar <edgaravetisyan932@gmail.com>
2023-03-17 20:52:26 +0400

    changes 0.00000030

--- pages2/technicsAccounting/tech.js
+++ pages2/technicsAccounting/tech.js
@@ -119,137 +119,82 @@ document
   .querySelector("#transaction-type-modal-btn")
   .addEventListener("click", () => showModal("#transaction-type-modal"));
 
-const addBtn = document.querySelector("#add-owner"),
-  addContainer = `
-     <div class="new_note_title">
-       <div>Սեփականատերեր</div>
-       <div></div>
-     </div>
-     <div class="note-container">
-       <div class="note-container-child">
-         <div class="select_mate line" data-mate-select="active" data-is-open="false">
-           <select name="" onchange="" onclick="return false;" id="">
-             <option value="">Անձի կարգավիճակ*</option>
-             <option value="1">Select option 1</option>
-             <option value="2">Select option 2</option>
-           </select>
-           <div class="select-title">
-             <p class="selecionado_opcion">Անձի կարգավիճակ*</p>
-             <span class="icon_select_mate icon" style="transform: rotate(0deg);"><img src="../../assets/select-chev.svg"></span>
-           </div>
-         <div class="cont_list_select_mate"><ul class="cont_select_int" style="height: 0px;"><li class="active" data-index="0">Անձի կարգավիճակ*</li><li data-index="1">Select option 1</li><li data-index="2">Select option 2</li></ul></div></div>   
-         <div class="input-group"><div class="input-box"><label class="main-label" for="ysFomeE">Անուն*</label><input class="input-wrap" placeholder="Անուն*" id="ysFomeE"></div>
+const addBtn = document.querySelector("#add-owner");
 
-         </div>   
-         <div class="input-group"><div class="input-box"><label class="main-label" for="vcrbEeT">Ազգանուն*</label><input class="input-wrap" placeholder="Ազգանուն*" id="vcrbEeT"></div>
-
-         </div>   
-         <div class="input-group"><div class="input-box"><label class="main-label" for="kfMPvGV">Հայրանուն*</label><input class="input-wrap" placeholder="Հայրանուն*" id="kfMPvGV"></div>
-
-         </div>
-         <div class="select_mate line" data-mate-select="active" data-is-open="false">
-           <select name="" onchange="" onclick="return false;" id="">
-             <option value="">Քաղաքացիություն*</option>
-             <option value="1">Select option 1</option>
-             <option value="2">Select option 2</option>
-           </select>
-           <div class="select-title">
-             <p class="selecionado_opcion">Քաղաքացիություն*</p>
-             <span class="icon_select_mate icon" style="transform: rotate(0deg);"><img src="../../assets/select-chev.svg"></span>
-           </div>
-         <div class="cont_list_select_mate"><ul class="cont_select_int" style="height: 0px;"><li class="active" data-index="0">Քաղաքացիություն*</li><li data-index="1">Select option 1</li><li data-index="2">Select option 2</li></ul></div></div>
-         <div class="select_mate line" data-mate-select="active" data-is-open="false">
-           <select name="" onchange="" onclick="return false;" id="">
-             <option value="">Փաստաթղթի տեսակը*</option>
-             <option value="1">Select option 1</option>
-             <option value="2">Select option 2</option>
-           </select>
-           <div class="select-title">
-             <p class="selecionado_opcion">Փաստաթղթի տեսակը*</p>
-             <span class="icon_select_mate icon" style="transform: rotate(0deg);"><img src="../../assets/select-chev.svg"></span>
-           </div>
-         <div class="cont_list_select_mate"><ul class="cont_select_int" style="height: 0px;"><li class="active" data-index="0">Փաստաթղթի տեսակը*</li><li data-index="1">Select option 1</li><li data-index="2">Select option 2</li></ul></div></div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="ToJbssO">Անձնագրի սերիա և համար*</label><input class="input-wrap" placeholder="Անձնագրի սերիա և համար*" id="ToJbssO"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="tTFpucG">Ում կողմից է տրված*</label><input class="input-wrap" placeholder="Ում կողմից է տրված*" id="tTFpucG"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="KrhecLn">Երբ է տրված*</label><input class="input-wrap" placeholder="Երբ է տրված*" id="KrhecLn"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="XyUHP_H">ՀԾՀ*</label><input class="input-wrap" placeholder="ՀԾՀ*" id="XyUHP_H"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="vZHSKFO">Ծննդյան ամսաթիվ*</label><input class="input-wrap" placeholder="Ծննդյան ամսաթիվ*" id="vZHSKFO"></div>
-
-         </div>
-         <div class="select_mate line" data-mate-select="active" data-is-open="false">
-           <select name="" onchange="" onclick="return false;" id="">
-             <option value="">Սեռ*</option>
-             <option value="1">Select option 1</option>
-             <option value="2">Select option 2</option>
-           </select>
-           <div class="select-title">
-             <p class="selecionado_opcion">Սեռ*</p>
-             <span class="icon_select_mate icon" style="transform: rotate(0deg);"><img src="../../assets/select-chev.svg"></span>
-           </div>
-         <div class="cont_list_select_mate"><ul class="cont_select_int" style="height: 0px;"><li class="active" data-index="0">Սեռ*</li><li data-index="1">Select option 1</li><li data-index="2">Select option 2</li></ul></div></div>
-         <div class="new-note-btn">Բեռնել</div>
-       </div>
-       <div class="note-container-child">
-         <div class="select_mate line" data-mate-select="active" data-is-open="false">
-           <select name="" onchange="" onclick="return false;" id="">
-             <option value="">Երկիր*</option>
-             <option value="1">Select option 1</option>
-             <option value="2">Select option 2</option>
-           </select>
-           <div class="select-title">
-             <p class="selecionado_opcion">Երկիր*</p>
-             <span class="icon_select_mate icon" style="transform: rotate(0deg);"><img src="../../assets/select-chev.svg"></span>
-           </div>
-         <div class="cont_list_select_mate"><ul class="cont_select_int" style="height: 0px;"><li class="active" data-index="0">Երկիր*</li><li data-index="1">Select option 1</li><li data-index="2">Select option 2</li></ul></div></div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="iAktlSX">Համայնք/Վարչ․շրջան*</label><input class="input-wrap" placeholder="Համայնք/Վարչ․շրջան*" id="iAktlSX"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="ufTPpyn">Բնակավայր*</label><input class="input-wrap" placeholder="Բնակավայր*" id="ufTPpyn"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="rtRtkhq">Փոստային դասիչ</label><input class="input-wrap" placeholder="Փոստային դասիչ" id="rtRtkhq"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="mHUJQLM">Փողոց</label><input class="input-wrap" placeholder="Փողոց" id="mHUJQLM"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="iOqgBFj">Տան տեսակ</label><input class="input-wrap" placeholder="Տան տեսակ" id="iOqgBFj"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="FmfRIOc">Շենք*</label><input class="input-wrap" placeholder="Շենք*" id="FmfRIOc"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="ENyRUip">Բնակարան/Սենյակ</label><input class="input-wrap" placeholder="Բնակարան/Սենյակ" id="ENyRUip"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="ExBdQUJ">Բաժնեմասեր</label><input class="input-wrap" placeholder="Բաժնեմասեր" id="ExBdQUJ"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="A_Dojhz">Հեռախոսահամար*</label><input class="input-wrap" placeholder="Հեռախոսահամար*" id="A_Dojhz"></div>
-
-         </div>
-         <div class="input-group"><div class="input-box"><label class="main-label" for="hToGXUc">Էլ․փոստ</label><input class="input-wrap" placeholder="Էլ․փոստ" id="hToGXUc"></div>
-
-         </div>
-       </div>
-     </div>
-                    
-  `;
-
+let note_count = 1;
 function addOwner() {
+  const addContainer = `
+  <div class="new_note_title"><div>Այլ սեփականատեր</div><div class="note-title-btnBox"><button type="button" class="note-container-delete">&#x2715;</button></div></div>
+  <div class="note-container">
+    <div class="note-container-child">
+      <div class="select_mate line" data-mate-select="active">
+        <select name="${randomId()}" onchange="" onclick="return false;"><option value="">Անձի կարգավիճակ*</option><option value="1">Select option 1</option><option value="2">Select option 2</option></select>
+        <div class="select-title"><p class="selecionado_opcion"></p><span class="icon_select_mate icon"><img src="../../assets/select-chev.svg"/></span></div>
+      </div>
+      <div class="input-group"><input name="${randomId()}" class="input-wrap" placeholder="Անուն*" /></div>
+      <div class="input-group"><input name="${randomId()}" class="input-wrap" placeholder="Ազգանուն*" /></div>
+      <div class="input-group"><input name="${randomId()}" class="input-wrap" placeholder="Հայրանուն*" /></div>
+      <div class="select_mate line" data-mate-select="active">
+        <select name="${randomId()}" onchange="" onclick="return false;"><option value="">Քաղաքացիություն*</option><option value="1">Select option 1</option><option value="2">Select option 2</option></select>
+        <div class="select-title"><p class="selecionado_opcion"></p><span class="icon_select_mate icon"><img src="../../assets/select-chev.svg"/></span></div>
+      </div>
+      <div class="select_mate line" data-mate-select="active">
+        <select name="${randomId()}" onchange="" onclick="return false;"><option value="">Փաստաթղթի տեսակը*</option><option value="1">Select option 1</option><option value="2">Select option 2</option></select>
+        <div class="select-title"><p class="selecionado_opcion"></p><span class="icon_select_mate icon"><img src="../../assets/select-chev.svg"/></span></div>
+      </div>
+      <div class="input-group"><input class="input-wrap" placeholder="Անձնագրի սերիա և համար*" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Ում կողմից է տրված*" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Երբ է տրված*" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="ՀԾՀ*" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Ծննդյան ամսաթիվ*" /></div>
+      <div class="select_mate line" data-mate-select="active">
+        <select onchange="" onclick="return false;"><option value="">Սեռ*</option><option value="1">Select option 1</option><option value="2">Select option 2</option></select>
+        <div class="select-title"><p class="selecionado_opcion"></p><span class="icon_select_mate icon"><img src="../../assets/select-chev.svg"/></span></div>
+      </div>
+      <div class="new-note-btn">Բեռնել</div>
+    </div>
+    <div class="note-container-child">
+      <div class="select_mate line" data-mate-select="active">
+        <select onchange="" onclick="return false;"><option value="">Երկիր*</option><option value="1">Select option 1</option><option value="2">Select option 2</option></select>
+        <div class="select-title"><p class="selecionado_opcion"></p><span class="icon_select_mate icon"><img src="../../assets/select-chev.svg"/></span></div>
+      </div>
+      <div class="input-group"><input class="input-wrap" placeholder="Համայնք/Վարչ․շրջան*" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Բնակավայր*" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Փոստային դասիչ" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Փողոց" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Տան տեսակ" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Շենք*" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Բնակարան/Սենյակ" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Բաժնեմասեր" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Հեռախոսահամար*" /></div>
+      <div class="input-group"><input class="input-wrap" placeholder="Էլ․փոստ" /></div>
+    </div>
+  </div>
+  `;
   const box = document.createElement("div");
   box.className = "new_note_container";
+  box.setAttribute("data-box-count", note_count);
   box.innerHTML = addContainer;
   const cont = this.closest(".new_note").querySelector(".note-container-box");
   cont.append(box);
+  note_count++;
+
+  createInputs(box);
+  createSelectOptions(box);
+  
+  box.querySelectorAll(".select-title").forEach((el) => {
+    el.addEventListener("click", openSelect);
+  });
+  box
+    .querySelector(".note-title-btnBox > button")
+    .addEventListener("click", function () {
+      this.closest(".new_note_container").remove();
+    });
 }
 
 addBtn.addEventListener("click", addOwner);
+
+document.querySelector("#the_form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(Object.fromEntries(new FormData(e.target)));
+});
